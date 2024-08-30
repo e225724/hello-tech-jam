b30cff1b0babc6292b8fd082bc8eede3b2847079
ShinjuHirayasu <hira.shin511@gmail.com>
2024-08-30 04:56:32 +0000

    gourmets-ccの一覧画面の表示を変更しました。

--- app/gourmets-cc/client.tsx
+++ app/gourmets-cc/client.tsx
@@ -39,7 +39,7 @@ const GourmetsClient = ({ initialShops }: { initialShops: Shop[] }) => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-start min-h-screen pt-36 px-8 md:px-12 lg:px-16">
+    <div className="flex flex-col items-end justify-start min-h-screen pt-12 px-8 md:px-12 lg:px-16">
       <div className="absolute top-4 right-4">
         <ThemeToggle />
       </div>
@@ -47,16 +47,16 @@ const GourmetsClient = ({ initialShops }: { initialShops: Shop[] }) => {
         onSubmit={handleSearch}
         className="flex items-center space-x-4 mb-8"
       >
-        <Input
+        {/* <Input
           type="search"
           value={keyword}
-          onChange={(e) => setKeyword(e.target.value)}
+          onChange={(e) => setKeyword(e.target.value)} //「検索」から「戻る」ボタンに変えます
           placeholder="検索..."
           className="max-w-sm w-full"
-        />
-        <Button type="submit">検索</Button>
+        /> */}
+        <Button type="submit">戻る</Button>
       </form>
-      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 w-full">
+      <div className="grid gap-3 md:grid-cols-3 lg:grid-cols-4 w-full">
         {shops.length > 0 ? (
           // shops.map((shop) => (
           //   <Card key={shop.id}>
@@ -76,15 +76,15 @@ const GourmetsClient = ({ initialShops }: { initialShops: Shop[] }) => {
           shops.map((shop) => (
             <Link key={shop.id} href={`gourmets-cc/${shop.id}`}>
               
-                <Card>
-                  <CardHeader className="space-y-4 p-6">
-                    <Avatar className="w-12 h-12">
-                      <AvatarImage src={shop.photo.pc.m} />
+                <Card className="h-80 overflow-auto ">
+                  <CardHeader className="space-y-4 p-3">
+                    <Avatar className="w-40 h-36 items-center">
+                      <AvatarImage src={shop.photo.pc.l} />
                       <AvatarFallback>CN</AvatarFallback>
                     </Avatar>
-                    <CardTitle>{shop.name}</CardTitle>
+                    <CardTitle className="text-lg">{shop.name}</CardTitle>
                   </CardHeader>
-                  <CardContent>
+                  <CardContent className="text-sm">
                     <p>{shop.address || "住所情報なし"}</p>
                     <p>{shop.genre?.name || "ジャンル情報なし"}</p>
                   </CardContent>
