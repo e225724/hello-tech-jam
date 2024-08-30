23585e123e4ed22c6929156d81c6d4674647d4de
ShinjuHirayasu <hira.shin511@gmail.com>
2024-08-30 05:42:59 +0000

    next.config.jsに変更はいりました

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'imgfp.hotp.jp',  // 許可するホスト名を追加
+          port: '',                   // 必要に応じてポート番号を指定
+          pathname: '/**',            // パスのパターンを指定、すべてのパスを許可する場合は '/**'
+        },
+      ],
+    },
+  }
+  
+  module.exports = nextConfig;
+  
